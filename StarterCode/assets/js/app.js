2b37b3908d6f39c8839cc91c22e23427851cb1b5
swood15 <41810698+swood15@users.noreply.github.com>
2018-12-14 23:50:53 -0700

    done

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -12,7 +12,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".scatter")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -27,7 +27,7 @@ d3.csv("../data/data.csv", function(err, data) {
   // Step 1: Parse Data/Cast as numbers
    // ==============================
   data.forEach(function(state) {
-    log.console(state.state)
+    console.log(state.state)
     state.poverty = +state.poverty;
     state.healthcare = +state.healthcare;
     // state.income = +state.income;
@@ -38,11 +38,11 @@ d3.csv("../data/data.csv", function(err, data) {
   // Step 2: Create scale functions
   // ==============================
   var xLinearScale = d3.scaleLinear()
-    .domain([20, d3.max(data, d => d.poverty)])
+    .domain([d3.min(data, d => d.poverty)-1, d3.max(data, d => d.poverty)+1])
     .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.healthcare)])
+    .domain([d3.min(data, d => d.healthcare)-1, d3.max(data, d => d.healthcare)+1])
     .range([height, 0]);
 
   // Step 3: Create axis functions
@@ -62,13 +62,16 @@ d3.csv("../data/data.csv", function(err, data) {
    // Step 5: Create Circles
   // ==============================
   var circlesGroup = chartGroup.selectAll("circle")
-  .data(hairData)
+  .data(data)
   .enter()
   .append("circle")
   .attr("cx", d => xLinearScale(d.poverty))
   .attr("cy", d => yLinearScale(d.healthcare))
-  .attr("r", "15")
+  .attr("r", "10")
   .attr("fill", "red")
+  .attr("text", "VA")
+  .attr("stroke", "black")
+  .attr("stroke-width", "1")
   .attr("opacity", ".5");
 
   // Step 6: Initialize tool tip
@@ -101,10 +104,10 @@ d3.csv("../data/data.csv", function(err, data) {
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .attr("class", "axisText")
-    .text("Number of Billboard 100 Hits");
+    .text("% w/o Healthcare");
 
   chartGroup.append("text")
     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
     .attr("class", "axisText")
-    .text("Hair Metal Band Hair Length (inches)");
+    .text("% in Poverty");
 });
